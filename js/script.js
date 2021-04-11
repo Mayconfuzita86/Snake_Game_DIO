9df69284025204635f8e7153eb46a701dea6f1fa
mayconfuzita <mayconfuzita@gmail.com>
2021-04-11 13:52:26 -0300

    style

--- js/script.js
+++ js/script.js
@@ -44,6 +44,13 @@ function update(event){
 
 function startGame(){
 
+    for(i = 1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert('GAME OVER... :( \n Press F5 to RESTART GAME!!!!')
+        }
+    }
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
